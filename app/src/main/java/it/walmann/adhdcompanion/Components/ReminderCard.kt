40869bf0c9eadf4ebfe87300f51b2a4049fcbd91
Tov Are Walmann <tovare@walmann.it>
2024-05-29 08:35:33 +0200

    Cleaned Import + code format.

--- app/src/main/java/it/walmann/adhdcompanion/Components/ReminderCard.kt
+++ app/src/main/java/it/walmann/adhdcompanion/Components/ReminderCard.kt
@@ -2,58 +2,36 @@ package it.walmann.adhdcompanion.Components
 
 import android.content.Context
 import android.graphics.BitmapFactory
-import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TimePicker
-import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.net.toUri
-import coil.compose.AsyncImage
 import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
-import coil.request.ImageRequest
-import it.walmann.adhdcompanion.Handlers.Settings.AppSettings
-import it.walmann.adhdcompanion.Handlers.Settings.getAppSetting
-import it.walmann.adhdcompanion.Handlers.Settings.setAppSetting
+import it.walmann.adhdcompanion.CupcakeScreen
+import it.walmann.adhdcompanion.MainActivity
 import it.walmann.adhdcompanion.MyObjects.reminder
 import it.walmann.adhdcompanion.R
-import it.walmann.adhdcompanion.ui.theme.ADHDCompanionTheme
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import java.io.File
 import java.util.Calendar
 
@@ -91,7 +69,12 @@ fun ReminderCard(
     val RemindImage = if (imgFile.exists()) {
         rememberAsyncImagePainter(imgFile)
     } else {
-        rememberAsyncImagePainter(BitmapFactory.decodeResource(context.resources, R.drawable.placeholder_reminderimage))
+        rememberAsyncImagePainter(
+            BitmapFactory.decodeResource(
+                context.resources,
+                R.drawable.placeholder_reminderimage
+            )
+        )
     }
 
 
