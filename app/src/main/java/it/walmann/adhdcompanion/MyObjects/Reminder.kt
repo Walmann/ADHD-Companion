2a59fb43846fda863a59c1632aafba56b5841a5e
Tov Are Walmann <tovare@walmann.it>
2024-05-31 14:03:59 +0200

    Remove unecesary code

--- app/src/main/java/it/walmann/adhdcompanion/MyObjects/Reminder.kt
+++ app/src/main/java/it/walmann/adhdcompanion/MyObjects/Reminder.kt
@@ -33,37 +33,3 @@ data class reminder(
         }
     }
 }
-
-
-@TypeConverters(CalendarConverter::class)
-class myReminder(
-    var reminderCalendar: Calendar = Calendar.getInstance(),
-    var reminderKey: String = reminderCalendar.timeInMillis.toString(),
-    val reminderImage: Uri = Uri.EMPTY,
-    val reminderNote: String = "You have a new reminder!",
-//    val reminderCreationCalendar: Calendar = Calendar.getInstance()
-)
-
-class CalendarConverter {
-    @TypeConverter
-    fun fromReminderCalendarToLong(calendar: Calendar): Long {
-        val newCal = Calendar.getInstance()
-        return newCal.timeInMillis
-    }
-
-    @TypeConverter
-    fun fromLongToReminderCalendar(long: Long): Calendar {
-        val newCalendar = Calendar.getInstance()
-        newCalendar.setTimeInMillis(long)
-        return newCalendar
-    }
-
-    @TypeConverter
-    fun toString(reminder: myReminder): String {
-        val new =
-            "${reminder.reminderKey},${reminder.reminderCalendar},${reminder.reminderImage},${reminder.reminderNote}"
-        return new
-    }
-
-
-}
