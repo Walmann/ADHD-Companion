59ece1ca4904cc054ecdf36805654a60aaf65330
Tov Are Walmann <tovare@walmann.it>
2024-06-05 13:22:35 +0200

    Cleanup unused code

--- app/src/main/java/it/walmann/adhdcompanion/Screens/CameraView.kt
+++ app/src/main/java/it/walmann/adhdcompanion/Screens/CameraView.kt
@@ -2,7 +2,6 @@ package it.walmann.adhdcompanion.Screens
 
 import android.content.Context
 import android.net.Uri
-import androidx.camera.core.ImageCaptureException
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,44 +13,27 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.ujizin.camposer.state.CamSelector
-import com.ujizin.camposer.state.CameraState
-import com.ujizin.camposer.state.ImageCaptureResult
-import com.ujizin.camposer.state.rememberCamSelector
-import com.ujizin.camposer.state.rememberCameraState
 import it.walmann.adhdcompanion.Components.myCamera.MyCameraPreview
 import it.walmann.adhdcompanion.R
 import it.walmann.adhdcompanion.requestPermissionCamera
 import java.io.File
 import java.util.Calendar
-import java.util.concurrent.Executor
-
 
 
 @Composable
 fun CameraView(
     modifier: Modifier = Modifier,
-    executor: Executor,
+//    executor: Executor,
     onImageCaptured: (Uri) -> Unit = {},
-    onError: (ImageCaptureException) -> Unit = {},
+//    onError: (ImageCaptureException) -> Unit = {},
     context: Context,
-    outputDirectory: File,
+//    outputDirectory: File,
 ) {
-
-    val cameraState = rememberCameraState()
-    var camSelector by rememberCamSelector(CamSelector.Back)
-
-
-//    if (requestPermission(Manifest.permission.CAMERA)) {
     if (requestPermissionCamera(context)) {
         Column(
             verticalArrangement = Arrangement.SpaceBetween,
@@ -100,26 +82,4 @@ fun CameraScreenButton(
             )
         }
     )
-}
-
-fun Context.createNewFile() = File(
-    filesDir, "${System.currentTimeMillis()}.jpg"
-).apply { createNewFile() }
-
-private fun myTakePhoto(
-    cameraState: CameraState,
-    context: Context,
-    onImageCaptured: (Uri) -> Unit,
-) {
-    cameraState.takePicture(
-        context.createNewFile(),
-        ) { result ->
-        if (result is ImageCaptureResult.Success) {
-
-            onImageCaptured(result.savedUri!!)
-        } else {
-            Throwable("Error capturing image!")
-        }
-
-    }
 }
